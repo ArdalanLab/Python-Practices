daa0c694b509d4df04e610c831c373f2a0de02b2
Ardalan Alizadeh <ardalan.alizadeh.laboratory@gmail.com>
2026-09-20 11:08:56 +0100

    Delete GT position calculator.py

--- GT position calculator.py
+++
@@ -1,7 +0,0 @@
-
-#script to give out the position of every "gt" in the sequence
-rna = input("Enter your sequence: ")
-
-for i in range(len(rna) - 1):
-    if rna[i:i+2] == "AUG":
-        print(f"'AUG' found at position {i}")
